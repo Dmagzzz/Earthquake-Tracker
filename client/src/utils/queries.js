383b729e68a5f6eeccec96956b023610d0da44a1
cbazil114 <113719887+cbazil114@users.noreply.github.com>
2023-03-21 14:39:06 -0400

    Client files/folders removed and cleaned up. Schema files in progress

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,52 +1,5 @@
 import { gql } from '@apollo/client';
 
-
-// export const QUERY_FRIENDS = gql`
-//   query getFriend($category: ID) {
-//     user(category: $category) {
-//       _id
-//      firstName
-//      lastName
-//      latitude
-//      longitude
-//      altitude
-//      userStatus
-//     }
-//   }
-// `;
-
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ID]!) {
-    checkout(products: $products) {
-      session
-    }
-  }
-`;
-
-export const QUERY_ALL_PRODUCTS = gql`
-  {
-    products {
-      _id
-      name
-      description
-      price
-      quantity
-      category {
-        name
-      }
-    }
-  }
-`;
-
-export const QUERY_CATEGORIES = gql`
-  {
-    categories {
-      _id
-      name
-    }
-  }
-`;
-
 export const QUERY_USER = gql`
   {
     user {
